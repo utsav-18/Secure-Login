8c8b671cf8ad08bb51880103449f84fdaf542aa5
Utsav Raj <utsavrajdocx@gmail.com>
2026-03-27 22:25:42 +0530

    MongoDB conneted

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch(err => console.log(err));
 
 // Dummy login route
 app.post("/login", (req, res) => {
